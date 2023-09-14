bba4f8c4a12be9fc28b6f2d276111c39a8101cf7
aleex <alee.xx1987@gmail.com>
2023-09-14 15:49:25 +0300

    Fix solution

--- src/Lesson01/DataTypsPrimitives.java
+++ src/Lesson01/DataTypsPrimitives.java
@@ -5,11 +5,16 @@ import java.sql.SQLOutput;
 public class DataTypsPrimitives {
     public static void main(String[] args) {
         int age= 36;
-        char FirstChar = 'A';
+        char FirstLetter = 'A';
         double avr = 80.5;
-        boolean isMale =true;
-        System.out.println("Myage "+age);
-        System.out.println( "Firstletter "+FirstChar);
+        boolean isFeMale =false;
+        System.out.println("My age is-"+age);
+        System.out.println( "My First letter is- "+FirstLetter);
+        System.out.println("My Avr- "+avr);
+        System.out.println("Am I a Female- "+isFeMale);
+        System.out.println(age+ " " +FirstLetter + " " +avr + " "+isFeMale + " ");
+
+
 
     }
 
