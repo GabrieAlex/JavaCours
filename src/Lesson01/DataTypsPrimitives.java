a51a152c29520112c9e7b759f06a0ecb7f4b383e
aleex <alee.xx1987@gmail.com>
2023-09-14 09:45:58 +0300

    NewSolution

---
+++ src/Lesson01/DataTypsPrimitives.java
@@ -0,0 +1,18 @@
+package Lesson01;
+
+import java.sql.SQLOutput;
+
+public class DataTypsPrimitives {
+    public static void main(String[] args) {
+        int age= 36;
+        char FirstChar = 'A';
+        double avr = 80.5;
+        boolean isMale =true;
+        System.out.println("Myage "+age);
+        System.out.println( "Firstletter "+);
+
+    }
+
+
+
+}
