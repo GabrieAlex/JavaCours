7e291da2a41557dc75b069af6a45768dae285468
aleex <alee.xx1987@gmail.com>
2023-09-17 11:40:17 +0300

    DataTypesNonePrimitives02

---
+++ src/Lesson01/DataTypesNonePrimitives03.java
@@ -0,0 +1,19 @@
+package Lesson01;
+
+public class DataTypesNonePrimitives03 {
+    public static void main(String[] args) {
+
+        String [] names = {"Gabriel","Mira","Ira","Ella","Yulia","Alex"};
+        String result = (names[0]+" "+names[1]+" "+names[2]+" "+names[3]+" "+names[4]+" "+names[5]+" ");
+        System.out.println(result.toUpperCase());
+        System.out.println(names[5].length());
+        System.out.println(names[0]+" "+names[1]);
+        System.out.println(names[0].charAt(0));
+        System.out.println(names[1].charAt(0));
+        System.out.println(names[2].charAt(0));
+        System.out.println(names[3].charAt(0));
+        System.out.println(names[4].charAt(0));
+        System.out.println(names[5].charAt(0));
+
+    }
+}
