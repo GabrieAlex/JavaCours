1da39fa2dbae5c860e121f8e22f489913e54701a
aleex <alee.xx1987@gmail.com>
2023-09-17 11:54:45 +0300

    DataTypesNonePrimitives02

---
+++ src/Lesson01/DataTypesNonePrimitives04.java
@@ -0,0 +1,15 @@
+package Lesson01;
+
+public class DataTypesNonePrimitives04 {
+    public static void main(String[] args) {
+
+        String [][] array = new String[2][3];
+        array [0][0]  = "Moshe";
+        array [0][1] = "22";
+        array [0][2] = "Jerusalem";
+        array [1][0] = "Joseph";
+        array [1][1] = "45";
+        array [1][2] = "London";
+        System.out.println(array[0][2]);
+    }
+}
