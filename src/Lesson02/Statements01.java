facc12c1f73e51f61e2b2e7d7a11a61ced88a723
aleex <alee.xx1987@gmail.com>
2023-09-23 21:15:32 +0300

    Statements01

---
+++ src/Lesson02/Statements01.java
@@ -0,0 +1,19 @@
+package Lesson02;
+
+public class Statements01 {
+    public static void main(String[] args)
+    {
+        double x = 5.18;
+        double y = 5.98;
+
+        if (x>y)
+            System.out.println(x);
+           else if (y>x)
+                System.out.println(y);
+            else
+                System.out.println(x+y);
+
+
+
+    }
+}
