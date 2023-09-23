e720e1a5fb1cf03ee365afcd7f79d1b97ca5cd0e
aleex <alee.xx1987@gmail.com>
2023-09-23 20:04:44 +0300

    Statements

---
+++ src/Lesson02/Statements.java
@@ -0,0 +1,14 @@
+package Lesson02;
+
+public class Statements {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+            if (age > 22)
+            System.out.println("you can vote");
+        else
+            System.out.println("you can't vote");
+
+    }
+}
