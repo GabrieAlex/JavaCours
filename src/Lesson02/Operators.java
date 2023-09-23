4a0485b4d3bf3272db2e93e7c796769487cb639e
aleex <alee.xx1987@gmail.com>
2023-09-23 20:02:19 +0300

    Operators

---
+++ src/Lesson02/Operators.java
@@ -0,0 +1,11 @@
+package Lesson02;
+
+public class Operators {
+    public static void main(String[] args) {
+        int number = 5;
+        int result = number % 2 == 0 ? number : -1;
+        System.out.println(result);
+
+
+    }
+}
