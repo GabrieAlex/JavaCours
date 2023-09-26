92454f6be6f3ef43720e47cee781e0b20353b8fe
aleex <alee.xx1987@gmail.com>
2023-09-26 22:46:59 +0300

    Statements05.java

---
+++ src/Lesson02/Statements05.java
@@ -0,0 +1,24 @@
+package Lesson02;
+
+public class Statements05 {
+    public static void main(String[] args)
+    {
+        int age =67;
+
+        if (age>0 && age<=6)
+            System.out.println("Go to kindergarten");
+
+        else if (7 <=age && age <=18)
+            System.out.println("GO to school");
+
+        else if (19<= age && age <= 67)
+            System.out.println("Go to work");
+
+        else if (67 <= age && age <=120)
+        System.out.println("Go to pension");
+
+
+
+
+    }
+}
