69b7ac6c93acf5b36ad8f9c975c95bb5c85d5124
aleex <alee.xx1987@gmail.com>
2023-09-27 18:03:22 +0300

    Statements10.java

---
+++ src/Lesson02/Statements10.java
@@ -0,0 +1,14 @@
+package Lesson02;
+
+public class Statements10 {
+    public static void main(String[] args) {
+
+        String [] array  = {"o","l","l","e","H"};
+        int count = array.length-1;
+        do {
+            System.out.print(array[count]);
+            count--;
+        }
+            while (count>=0);
+    }
+}
